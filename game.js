88f326aecb3bedc896c5d6f73f67614491d5b458
radmx <cclaros2024@straphaelschoolsb.org>
2025-05-07 17:33:30 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,77 +1,79 @@
-// Asset loading
-const assets = {
-    player: new Image(),
-    grass: new Image(),
-    // You can add more: tree: new Image(), etc.
-};
-assets.player.src = 'images/player.gif'; // or player.png (spritesheet preferred)
-assets.grass.src = 'images/grass.png';
-
-// Canvas setup
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
-
-// Resize canvas
-function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
-window.addEventListener('resize', resizeCanvas);
-resizeCanvas();
-
-// Game state
-let player = { x: 200, y: 200, dir: 'down' };
-let inBattle = false;
-let grassPatches = [];
-
-// Initialization
-function init() {
-    createGrass();
-    document.addEventListener('keydown', handleKeyPress);
-    gameLoop();
-}
-
-// Game loop
-function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawGrass();
-    drawPlayer();
-    if (inBattle) drawBattle();
-    requestAnimationFrame(gameLoop);
-}
-
-function drawPlayer() {
-    ctx.drawImage(assets.player, player.x - 16, player.y - 16, 32, 32); // Centered
-}
-
-function createGrass() {
-    for (let i = 0; i < 15; i++) {
-        grassPatches.push({
-            x: Math.random() * canvas.width,
-            y: Math.random() * canvas.height,
-            width: 64,
-            height: 64
-        });
+const config = {
+    type: Phaser.AUTO,
+    width: 256,
+    height: 256,
+    pixelArt: true,
+    scene: {
+        preload,
+        create,
+        update
     }
-}
+};
 
-function drawGrass() {
-    grassPatches.forEach(patch => {
-        ctx.drawImage(assets.grass, patch.x, patch.y, patch.width, patch.height);
+const game = new Phaser.Game(config);
+
+let player, cursors;
+
+function preload() {
+    this.load.spritesheet('hero', 'sCrkzvs.png', {
+        frameWidth: 64,
+        frameHeight: 64
     });
 }
 
-function handleKeyPress(e) {
-    const speed = 5;
-    if (e.key === 'ArrowUp') { player.y -= speed; player.dir = 'up'; }
-    if (e.key === 'ArrowDown') { player.y += speed; player.dir = 'down'; }
-    if (e.key === 'ArrowLeft') { player.x -= speed; player.dir = 'left'; }
-    if (e.key === 'ArrowRight') { player.x += speed; player.dir = 'right'; }
+function create() {
+    player = this.add.sprite(128, 128, 'hero');
+
+    this.anims.create({
+        key: 'walk_down',
+        frames: this.anims.generateFrameNumbers('hero', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'walk_left',
+        frames: this.anims.generateFrameNumbers('hero', { start: 4, end: 7 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'walk_right',
+        frames: this.anims.generateFrameNumbers('hero', { start: 8, end: 11 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'walk_up',
+        frames: this.anims.generateFrameNumbers('hero', { start: 12, end: 15 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
-function drawBattle() {
-    ctx.fillStyle = 'red';
-    ctx.fillRect(canvas.width / 2 - 100, canvas.height / 2 - 100, 200, 200);
-}
+function update() {
+    let moving = false;
 
-init();
+    if (cursors.left.isDown) {
+        player.x -= 2;
+        player.anims.play('walk_left', true);
+        moving = true;
+    } else if (cursors.right.isDown) {
+        player.x += 2;
+        player.anims.play('walk_right', true);
+        moving = true;
+    } else if (cursors.up.isDown) {
+        player.y -= 2;
+        player.anims.play('walk_up', true);
+        moving = true;
+    } else if (cursors.down.isDown) {
+        player.y += 2;
+        player.anims.play('walk_down', true);
+        moving = true;
+    }
+
+    if (!moving) {
+        player.anims.stop();
+    }
+}
