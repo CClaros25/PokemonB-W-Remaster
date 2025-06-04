5b9c9b35a62e0c5aff31ecea7f45306ee8fd8872
radmx <cclaros2024@straphaelschoolsb.org>
2025-06-03 20:17:23 -0700

    Update game.js

--- game.js
+++ game.js
@@ -309,8 +309,8 @@ frameRate: 1
 
 // ===== SIDE PANEL =====
 function createSidePanel() {
-  const iconWidth = 244; // half of 487
-  const iconHeight = 116; // half of 231
+  const iconWidth = 292; // half of 487
+  const iconHeight = 139; // half of 231
   const cellSizeX = iconWidth + 20; // icon width + gap
   const cellSizeY = iconHeight + 20; // icon height + gap
   const startX = 40;
